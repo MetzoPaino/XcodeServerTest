0c0e6bb21905fe96c69fb6d17b799b0e09d5e441
William <williamjrobinson@me.com>
2019-01-24 09:44:43 +0000

    Initial Commit

---
+++ XcodeServerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeServerTest
+//
+//  Created by William Robinson on 24/01/2019.
+//  Copyright © 2019 WR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
