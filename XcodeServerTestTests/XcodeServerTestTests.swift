23f836851f0b437652af17e66b119fb366f67728
William <williamjrobinson@me.com>
2019-01-24 14:12:33 +0000

    Recommit

--- XcodeServerTestTests/XcodeServerTestTests.swift
+++ XcodeServerTestTests/XcodeServerTestTests.swift
@@ -22,6 +22,7 @@ class XcodeServerTestTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTru(true)
     }
 
     func testPerformanceExample() {
